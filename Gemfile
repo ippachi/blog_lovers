f8655d9201c7009788d59405b0a541428abd8c5a
ippachi <ivalice1218@gmail.com>
2017-10-29 02:40:10 +0000

    generate article model

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'slim'
 gem 'html2slim'
+gem 'annotate'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ test/models/article_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id             :integer          not null, primary key
+#  title          :string
+#  content        :text
+#  publish_status :boolean
+#  user_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
