e5f92b883b65049b3dec7f7ba89217af5a0aaefb
ippachi <ivalice1218@gmail.com>
2017-10-29 05:44:52 +0000

    arrange factories

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,24 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id             :integer          not null, primary key
+#  title          :string           not null
+#  content        :text             not null
+#  publish_status :boolean          default(TRUE), not null
+#  user_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 FactoryBot.define do
   factory :article do
-    
+    title 'test_title'
+    content 'test_content'
+    publish_status true
+
+    trait :with_user do
+      association :user, factory: :user
+    end
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id             :integer          not null, primary key
+#  title          :string           not null
+#  content        :text             not null
+#  publish_status :boolean          default(TRUE), not null
+#  user_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
