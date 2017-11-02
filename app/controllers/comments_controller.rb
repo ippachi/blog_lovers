9254d215c599b2d30ec0539540a6f1025e5ddafd
ippachi <ivalice1218@gmail.com>
2017-11-02 17:09:37 +0900

    modify comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
+  before_action :set_user
+  before_action :set_article
   def create
     @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      respond_to do |format|
+    respond_to do |format|
+      if @comment.save
         format.html
         format.js { @comments = Article.find(params[:article_id]).comments }
+      else
+        format.html { redirect_to user_article_url(@user, @article) }
       end
-    else
-      redirect_to :back
     end
   end
 
@@ -15,5 +17,13 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content, :article_id)
+  end
+
+  def set_user
+    @user = User.find_by(screen_name: params[:user_screen_name])
+  end
+
+  def set_article
+    @article = @user.articles.find(params[:article_id])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,4 +13,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  validates :content, presence: true
 end
